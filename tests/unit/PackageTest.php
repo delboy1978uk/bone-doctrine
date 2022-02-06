08013b6c4d2b8f3c4320b7c1cf04621a38b2caba
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-02-06 18:41:22 +0100

    adding tests

---
+++ tests/unit/PackageTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bone\Test\BoneDoctrine;
+
+use Barnacle\Container;
+use Bone\BoneDoctrine\BoneDoctrinePackage;
+use Codeception\Test\Unit;
+use Doctrine\ORM\EntityManager;
+
+class PackageTest extends Unit
+{
+    public function testPackage()
+    {
+        $container = new Container();
+        $container['cache_dir'] = './tests/data';
+        $container['proxy_dir'] = './tests/data';
+        $container['entity_paths'] = './tests/data';
+        $container['db'] = [
+            'host' => '127.0.0.1',
+            'dbname' => 'awesome',
+            'user' => 'root',
+            'password' => 'root'
+        ];
+        $package = new BoneDoctrinePackage();
+        $package->addToContainer($container);
+
+        $this->assertTrue($container->has(EntityManager::class));
+        $this->assertInstanceOf(EntityManager::class, $container->get(EntityManager::class));
+        $this->assertIsArray($package->registerConsoleCommands($container->get(EntityManager::class)));
+    }
+}
