d8bc867641bdae8ce44cbd06951b119d4d8af87b
Derek Stephen McLean <delboy1978uk@gmail.com>
2023-08-15 15:40:36 +0200

    Add has coordinates trait

---
+++ src/Traits/HasLocationCoordinates.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bone\BoneDoctrine\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait HasLocationCoordinates
+{
+    /** @ORM\Column(type="float", precision=17, scale=15, nullable=true) */
+    private float $longitude = 0.0;
+
+    /** @ORM\Column(type="float", precision=17, scale=15, nullable=true) */
+    private float $latitude = 0.0;
+
+    public function getLongitude(): float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): void
+    {
+        $this->longitude = $longitude;
+    }
+
+    public function getLatitude(): float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): void
+    {
+        $this->latitude = $latitude;
+    }
+}
